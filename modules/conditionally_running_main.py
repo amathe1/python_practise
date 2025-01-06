fe732594a207844342bf888c274543ec717b4ad7
amathe1 <amathe1@apple.com>
2025-01-06 17:03:51 +0530

    ok

---
+++ modules/conditionally_running_main.py
@@ -0,0 +1,8 @@
+
+import support_for_conditionally_running_main as scr
+
+def main():
+    scr.print_name()
+
+if __name__ == "__main__":
+    main()
---
+++ modules/support_for_conditionally_running_main.py
@@ -0,0 +1,8 @@
+def print_name():
+    print("Arun Mathe")
+
+def main():
+    print(__name__)
+
+if __name__ == "__main__":
+    main()
