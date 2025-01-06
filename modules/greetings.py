129802ddcfc65d83ba11cf88249643a78d8bc978
amathe1 <amathe1@apple.com>
2025-01-06 16:31:46 +0530

    ok

---
+++ modules/greetings.py
@@ -0,0 +1,7 @@
+
+
+def informal_greeting():
+    print("Hi....")
+
+def formal_greeting():
+    print("Good morning!")
---
+++ modules/modules_demo.py
@@ -0,0 +1,15 @@
+"""
+importing other python file/module using 'import' statement and creating a shortcut 'grt' for it
+"""
+
+import greetings as grt
+
+
+"""
+calling below methods() which are defined in other program 'greetings.py'
+"""
+def main():
+    grt.informal_greeting()
+    grt.formal_greeting()
+
+main()
